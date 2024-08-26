24167ddce0cdd90c015e2f5423b0823b02d3d404
David Arens <davidarens@admins-mbp.speedport.ip>
2024-08-26 22:05:01 +0200

    added username and used it for feedback url

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :drum_sessions
 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows letters, numbers, and underscores" }
+
   def active_session
     drum_sessions.find_by(active: true)
   end
