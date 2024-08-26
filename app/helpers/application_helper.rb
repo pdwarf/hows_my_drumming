825c5b4b1065745d89476981c904ff0ce47268a4
David Arens <davidarens@admins-mbp.speedport.ip>
2024-08-26 14:35:09 +0200

    print view pulled out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,17 @@
 require 'rqrcode'
 
 module ApplicationHelper
-  def qr_code_as_svg(url)
+  def qr_code_as_svg(url, options = {})
+    defaults = {
+        module_size: 7,
+        use_path: true
+    }
+
+    options = defaults.merge(options)
+
     qrcode = RQRCode::QRCode.new(url)
     
-    svg = qrcode.as_svg(
-      color: "000",
-      shape_rendering: "crispEdges",
-      module_size: 11,
-      standalone: true,
-      use_path: true
-    )
+    svg = qrcode.as_svg(options)
 
     svg.html_safe
   end
