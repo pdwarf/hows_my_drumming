2cae00b1ec4ded1f84f11898ed482e5fa0bce8ca
David Arens <davidarens@admins-mbp.speedport.ip>
2024-09-01 22:08:06 +0200

    sendgrid POC integration working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'drumfeedback.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
