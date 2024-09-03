546bf35b2b7f1abfa91effbda921fac14098ad2d
David Arens <davidarens@admins-mbp.speedport.ip>
2024-09-03 22:03:57 +0200

    menu done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,5 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import DropdownController from "./dropdown_controller"
+application.register("dropdown", DropdownController)
